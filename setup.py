a8158e57f73552b40996f1ca6f4c0ac0f9e748b2
SoufianeAmghar <soufiane35.live@gmail.com>
2025-03-07 16:45:29 +0000

    Updated to version 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cmr_connectors_lib',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     # Add dependencies here
     install_requires=[
