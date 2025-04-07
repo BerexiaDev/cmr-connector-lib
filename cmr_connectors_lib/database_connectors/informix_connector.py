d986325bdfded29426dca040201a649e3219af14
Karim Bourass <bourasskarim9@gmail.com>
2025-04-07 16:00:47 +0100

    Update InformixConnector to use UTF-8 encoding for character and metadata decoding, improving compatibility with various data formats.

--- cmr_connectors_lib/database_connectors/informix_connector.py
+++ cmr_connectors_lib/database_connectors/informix_connector.py
@@ -37,11 +37,10 @@ class InformixConnector(SqlConnector):
          )
         logger.info(f"Connection string: {conn_str}")
         conn = pyodbc.connect(conn_str)
-        conn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-16-le')
-        conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-16-le')
-        conn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-16-le')
-        conn.setencoding(str, encoding='utf-16-le')
-        conn.setencoding(str, encoding='utf-16-le')
+        conn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
+        conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
+        conn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-8')
+        conn.setencoding(str, encoding='utf-8')
         return conn
 
     def ping(self):
