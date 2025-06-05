46457218fda84d10348bca07b3b282f8d5594ed6
Karim Bourass <bourasskarim9@gmail.com>
2025-06-05 12:35:22 +0100

    Update: bump version to 0.10.0 and change author details in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cmr_connectors_lib',
-    version='0.8.7',
+    version='0.10.0',
     packages=find_packages(),
     # Add dependencies here
     install_requires=[
@@ -13,8 +13,8 @@ setup(
         "loguru",
     ],
     description='CMR Connectors Library',
-    author='Soufiane',
-    author_email='soufiane.amghar@berexia.com',
+    author='Berexia DEV Team',
+    author_email='berexiadev@berexia.com',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     classifiers=[
